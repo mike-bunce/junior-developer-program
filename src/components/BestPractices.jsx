8b2a1efc8a8bdbdc074877be0a6bd407f8e1b7bc
Mike Bunce <mike.bunce@telus.com>
2020-09-29 00:04:22 -0700

    colour tweaks

--- src/components/BestPractices.jsx
+++ src/components/BestPractices.jsx
@@ -7,7 +7,7 @@ const BestPractices = () => {
   return (
     <div className="front-page">
       <Header />
-      <h1>React Best Practices - part 2</h1>
+      <h1>React Best Practices 2</h1>
       <img src="./squares.gif" className="front-image" alt="" />
       <Link to="/DRY">Next</Link>
     </div>
