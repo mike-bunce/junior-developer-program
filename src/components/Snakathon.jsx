52745e2e7a88092d51381d8777e8b2ff750d6f6b
Mike Bunce <mike.bunce@telus.com>
2020-10-06 11:27:38 -0700

    WIP

--- src/components/Snakathon.jsx
+++ src/components/Snakathon.jsx
@@ -24,37 +24,24 @@ const Snakathon = () => {
       return e.keyCode;
     }
   };
-
   const [lengthOfSnake, setLengthOfSnake] = useState(3)
+  const [firstSquare, setFirstSquare] = useState({topEnd: 0, leftEnd: 0})
   const [snake, setSnake] = useState(['snake']);
   const handleSnakeGrow = (e) => {
     setLengthOfSnake(lengthOfSnake + 3)
     const snakeArray = snake;
     snakeArray.push('snake');
     setSnake(snakeArray);
-    // extendedSnake()
   }
-  // const extendedSnake = () => {
-  //   snake.map((item) => {
-  //   return (
-  //     <React.Fragment>
-  //       <SnakeSquare item={item} />
-  //       <SnakeSquare item={item} />
-  //       <SnakeSquare item={item} />
-  //     </React.Fragment>
-  //     )
-  //   })
-  // }
-  console.log('lengthOfSnake', lengthOfSnake)
-  console.log('snake', snake)
+  
   return (
     <div className="front-page" onKeyDown={handleKeyDown} tabIndex="0">
       <h1>Snakathon</h1>
       <Container>
           {snake.map(() => {return (<React.Fragment>
-          <SnakeSquare index="1" topStart="0px" topEnd="15px" leftStart="0px" leftEnd="0px" />
-          <SnakeSquare index="2" topStart="15px" topEnd="30px" leftStart="0px" leftEnd="0px" />
-          <SnakeSquare index="3" topStart="30px" topEnd="30px" leftStart="0px" leftEnd="15px" />
+            <SnakeSquare index="1" topStart={} topEnd={firstSquare.topEnd} leftStart="0px" leftEnd="0px" />
+            <SnakeSquare index="2" topStart="15px" topEnd="30px" leftStart="0px" leftEnd="0px" />
+            <SnakeSquare index="3" topStart="30px" topEnd="30px" leftStart="0px" leftEnd="15px" />
       </React.Fragment>)})}
       </Container>
       <button onClick={handleSnakeGrow}>Extend Snake</button>
