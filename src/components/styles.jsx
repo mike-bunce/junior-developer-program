36e52910b7ac0ff227e05991917388212ae1d6db
John <John.Twigg@telus.com>
2020-10-06 10:27:34 -0700

    Fix animation

--- src/components/styles.jsx
+++ src/components/styles.jsx
@@ -4,25 +4,35 @@ const Snake = styled.div`
     display: inline-block;
     padding: 5px;
     background-color: red;
-`
-const snakesquare = keyframes`
-    0% {
-        top: 0
-        right: 0
+    position: absolute;
+    animation: snake 0.3s 0s both;
+    @keyframes snake {
+        0% { top: 0 }
+        100% { top: 15px }
     }
+`
+// const snakesquare = keyframes`
+//     0% {
+//         top: 0;
+//         right: 0;
+//     }
 
-    100% {
-        top: 15px
-        right: 0px;
-    }
-`
+//     100% {
+//         top: 15px;
+//         right: 0px;
+//     }
+// `
 const SnakeSquare = styled.div`
     border: 2px solid #ffffff;
     width: 15px;
     height: 15px;
     background-color: green;
-    position: absolute;
-    animation: ${snakesquare} 0.3s 0s both;
+    position: abolute;
+    animation: snakesquare 0.3s 0s both;
+    @keyframes snakesquare {
+        0% { top: 0 }
+        100% { top: 15px }
+    }
 `
 const Container = styled.div`
     width: 495px;
